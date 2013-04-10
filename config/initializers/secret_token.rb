4938cd4e4cb893f3801f387fc2c45aabefb84591
francisco <fjavier.cgam@gmail.com>
2013-04-10 13:16:48 -0600

    Primer Subida de la segunda app. en ruby

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SecondApp::Application.config.secret_token = 'f4b5783b00151f5d2a72118b5c6b138e1c12c97727e029a16d6d7ad32364e02a7ba3309f213f063c7f1a541b06fbea4021d1634486a90c496c938de5f1867fd3'
